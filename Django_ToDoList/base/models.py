52a90dbc928adf42fa6bcb919b4d0e07b5b24a78
Dancha778 <dancha778@mail.ru>
2023-05-30 22:20:43 +0300

    adding models

--- Django_ToDoList/base/models.py
+++ Django_ToDoList/base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+class task(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null = True.blank=True)
+    title = models.CharField()
+    description =
+    complete =
+    create =
